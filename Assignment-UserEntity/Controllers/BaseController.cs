6dc8e552b09bfd5aa67ac47e0917da633b2dae3a
ahadullah8 <ahad.mujahid@hazelsoft.org>
2024-02-15 13:30:14 +0500

    base controller updated

--- Assignment-UserEntity/Controllers/BaseController.cs
+++ Assignment-UserEntity/Controllers/BaseController.cs
@@ -19,21 +19,6 @@ namespace Assignment_UserEntity.Controllers
             response.Success = true;
             return new OkObjectResult(response);
         }
-        /// <summary>
-        /// Creates an <see cref="OkObjectResult"/> object that produces an <see cref="StatusCodes.Status200OK"/> response.
-        /// </summary>
-        /// <param name="value">The content value to format in the entity body.</param>
-        /// <param name="message">The message to send back to the user as response message.</param>
-        /// <returns>The created <see cref="OkObjectResult"/> for the response.</returns>
-        protected OkObjectResult Ok([ActionResultObjectValue] object? value, string? message)
-        {
-            ResponseDto response = new();
-            response.StatusCode = StatusCodes.Status200OK;
-            response.Message = message;
-            response.Payload = value;
-            response.Success = true;
-            return new OkObjectResult(response);
-        }
 
         public override BadRequestObjectResult BadRequest([ActionResultObjectValue] object? value)
         {
